062a10962b0d190105911029c8b0f38571213d3b
Alex Yang <alex.yang@bayer.com>
2021-11-06 00:18:00 -0500

    complete 13.6.5

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -57,6 +57,38 @@ let overlays = {
 // pass our map layer into our layers control and add the layers control to the map
 L.control.layers(baseMaps, overlays).addTo(map);
 
+// Create a legend control object
+let legend = L.control({
+    position: 'bottomright'
+});
+
+// then add all the details for the legend
+// The legend will be added to a div element on the index.html file using the DomUtil utility function.
+legend.onAdd = function() {
+    let div = L.DomUtil.create('div', 'info legend');
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+    ];
+    
+    // looping through our intervals to generate a label with a colored square for each interval
+    for  (var i=0; i<magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML += 
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
 // Add GeoJSON data.
 // Accessing the airport GeoJSON URL
 let quakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
